eb177a0bc44790aa1d4b484e75df37dd637be582
Abbas Hooshmand <abbas.hooshmand@gmail.com>
2017-12-21 18:07:54 -0800

    Ran fake friends data set

--- SparkScalaCourse/src/com/abbashooshmand/spark/RatingsCounter.scala
+++ SparkScalaCourse/src/com/abbashooshmand/spark/RatingsCounter.scala
@@ -13,7 +13,7 @@ object RatingsCounter {
     // Set the log level to only print errors
     Logger.getLogger("org").setLevel(Level.ERROR)
         
-    // Create a SparkContext using every core of the local machine, named RatingsCounter
+    // Create a SparkContext using every core of the local machine (*), named RatingsCounter
     val sc = new SparkContext("local[*]", "RatingsCounter")
    
     // Load up each line of the ratings data into an RDD
